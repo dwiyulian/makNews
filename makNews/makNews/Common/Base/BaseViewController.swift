9e571f83707116d455442418a9ea436f5d4372e5
Rizki Saputra <dwiyulianrizkisaputra@gmail.com>
2022-04-16 07:59:44 +0700

    General - Add base view

---
+++ makNews/makNews/Common/Base/BaseViewController.swift
@@ -0,0 +1,18 @@
+//
+//  BaseViewController.swift
+//  makNews
+//
+//  Created by MacBook on 16/04/22.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
